1509856334bcf2badf90867b928a2e4a21fc18a9
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-03-17 10:59:30 +0100

    Update rrlist.sh

--- rrlist.sh
+++ rrlist.sh
@@ -4,5 +4,5 @@
 #drgrtlist=$(oci network drg-route-table list --all --drg-id $drgocid) 
 drgrtlist=$(oci network drg-route-table list --all --drg-id $1) 
 drgrtlistcur=$(echo $drgrtlist | jq .data | jq -r '.[] | ."id"')
-for drgrt in $drgrtlistcur; do oci network drg-route-table get --drg-route-table-id $drgrt --query "data.[\"display-name\"]" && oci network drg-route-rule list -all --drg-route-table-id $drgrt --output table; done
+for drgrt in $drgrtlistcur; do oci network drg-route-table get --drg-route-table-id $drgrt --query "data.[\"display-name\"]" && oci network drg-route-rule list --all --drg-route-table-id $drgrt --output table; done
 ###-----------------------------------
