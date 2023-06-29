d4ce61b38d78da200b304e60c8d4d01f9db3bc4d
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-06-29 02:20:53 +0200

    Create drgrtlist.sh

---
+++ drgrtlist.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+###--drgrtlist.sh--
+export drg=$1
+export region=$2
+drgobj=$(oci network drg get --drg-id $drg --region $region)
+drgname=$(echo $drgobj | jq -r '.data | ."display-name"')
+rm -f rrules-*.csv
+drgrtlist=$(oci network drg-route-table list --all --drg-id $drg --region $region) 
+drgrtlistcur=$(echo $drgrtlist | jq .data | jq -r '.[] | ."id"')
+
+for drgrtid in $drgrtlistcur; do echo "Enumerating Route Rules in" $region $drgname $drgrtid && ./rrlist.sh $drgrtid $region $drgname; done
+cat rrules-*.csv >> drgrtrules-$drgname-$region.csv
