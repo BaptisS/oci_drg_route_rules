b75f73ed63d2fbb4401fa4cee33436f4eed33f2f
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-06-29 02:20:26 +0200

    Create listdrgs.sh

---
+++ listdrgs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+### listdrgs.sh
+export region=$1
+rm -f drgrtrulesregion-$region.csv
+oci search resource structured-search --query-text "query drg resources" --region $region > drgs-$region.log
+drgocids=$(cat drgs-$region.log | jq -r '.data.items[] | ."identifier"')
+for drgid in $drgocids; do echo "Enumerating DRG Route Tables in" $region $drgid && ./drgrtlist.sh $drgid $region; done
+cat drgrtrules-*.csv >> drgrtrulesregion-$region.csv
+rm -f drgrtrules-*.csv
