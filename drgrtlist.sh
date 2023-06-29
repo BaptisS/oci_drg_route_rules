a73b939bdc2be38ecd10b836c0c73dc106b7d549
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-06-29 16:48:00 +0200

    Update drgrtlist.sh

--- drgrtlist.sh
+++ drgrtlist.sh
@@ -6,7 +6,7 @@ drgobj=$(oci network drg get --drg-id $drg --region $region)
 drgname=$(echo $drgobj | jq -r '.data | ."display-name"')
 rm -f rrules-*.csv
 drgrtlist=$(oci network drg-route-table list --all --drg-id $drg --region $region --query 'data[?!contains("display-name",'"'Auto'"')]') 
-drgrtlistcur=$(echo $drgrtlist | jq .data | jq -r '.[] | ."id"')
+drgrtlistcur=$(echo $drgrtlist | jq -r '.[] | ."id"')
 
 for drgrtid in $drgrtlistcur; do echo "Enumerating Route Rules in" $region $drgname $drgrtid && ./rrlist.sh $drgrtid $region $drgname; done
 cat rrules-*.csv >> drgrtrules-$drgname-$region.csv
